9c894df60be8f8ddc85386654573a6873e01a217
Tom <tom@inferis.org>
2025-01-09 12:57:29 -0800

    Implement block powered states.

--- src/main/java/wrs/block/ReceiverBlock.java
+++ src/main/java/wrs/block/ReceiverBlock.java
@@ -1,9 +1,30 @@
 package wrs.block;
 
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.state.StateManager;
+import net.minecraft.state.property.Properties;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraft.world.block.WireOrientation;
 
 public class ReceiverBlock extends Block {
     public ReceiverBlock(Settings settings) {
         super(settings);
+        setDefaultState(getDefaultState().with(Properties.POWERED, false));
+    }
+
+    @Override
+    protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
+        builder.add(Properties.POWERED);
+    }
+
+    @Override
+    protected void neighborUpdate(BlockState state, World world, BlockPos pos, Block sourceBlock,
+            WireOrientation wireOrientation, boolean notify) {
+        super.neighborUpdate(state, world, pos, sourceBlock, wireOrientation, notify);
+
+        var power = world.getReceivedRedstonePower(pos);
+        world.setBlockState(pos, state.with(Properties.POWERED, power > 0));
     }
 }
--- src/main/java/wrs/block/TransmitterBlock.java
+++ src/main/java/wrs/block/TransmitterBlock.java
@@ -1,9 +1,18 @@
 package wrs.block;
 
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.state.StateManager;
+import net.minecraft.state.property.Properties;
 
 public class TransmitterBlock extends Block {
     public TransmitterBlock(Settings settings) {
         super(settings);
-    }    
+        setDefaultState(getDefaultState().with(Properties.POWERED, false));
+    }
+
+    @Override
+    protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
+        builder.add(Properties.POWERED);
+    }
 }
